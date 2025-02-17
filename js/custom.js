d25a90fa9ef77f2ee8b157cf05d6b1f53a7842df
desipossa <c1jang@hanmail.net>
2025-02-17 14:42:23 +0900

    20250217

--- js/custom.js
+++ js/custom.js
@@ -1,6 +1,16 @@
 $(function () {
     const MVS = new Swiper('.main_visual_slide', {
         direction: 'vertical',
+        loop: true,
+        speed: 1000,
+        autoplay: {
+            delay: 4000,
+            disableOnInteraction: false,
+        },
+        pagination: {
+            el: ".main_visual .page",
+            clickable: true,
+        },
     })
 })
 
